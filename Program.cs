96170bc1a5b1bcd026815eeda2b81f7e7b8f5190
DevMashup <mashermasherr.buisness@gmail.com>
2023-03-02 10:00:24 -0800

    v1.0.0.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice {
+    class HelloWorld {
+        static void Main(string[] args) {
+            Console.WriteLine("Pleas enter your name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");        
+        }
+    }
+}
